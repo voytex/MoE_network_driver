ab1754b9e7f8f3db5d7f4e46f3efdf72e930d138
woeetech <vlukas@hotmail.cz>
2020-12-09 10:43:34 +0100

    auto broadcast_ip

--- MoE_driver.py
+++ MoE_driver.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import ipaddress
 
 class Device:
     def __init__(self):
@@ -43,7 +44,13 @@ devices = dict()
 
 MOE_PORT = 50000
 MY_IP = socket.gethostbyname(socket.gethostname())
-BROADCAST_IP = '192.168.2.255'
+MASK = '255.255.255.0'
+#BROADCAST_IP = '192.168.2.255'
+
+HOST = ipaddress.IPv4Address(MY_IP)
+NET = ipaddress.IPv4Network(MY_IP + '/' + MASK, False)
+
+BROADCAST_IP = str(NET.broadcast_address)
 
 out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 out.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -59,7 +66,7 @@ inp.bind((MY_IP, MOE_PORT))
 thread = threading.Thread(target=watcher)
 thread.start()
 
-out.sendto(b'\x08\x08\x08\x08', ('192.168.2.255', MOE_PORT))
+out.sendto(b'\x08\x08\x08\x08', (BROADCAST_IP, MOE_PORT))
 
 print("****** MoE Matrix Editor v0.5 ****** \n'print' \tsee devices and their connections on the network \n'add' \t\tadd connection, from one device to another \n'del' \t\tdelete connection\n'reload' \treload the session. (e.g. when Arduino was restarted)")
 #out.sendto(b'\xFF\xFF\xFF\xFF', ('192.168.2.116', MOE_PORT))
@@ -74,7 +81,7 @@ while True:
             devices[d].print_device()
     if query == 'reload':
         devices.clear()
-        out.sendto(b'\x08\x08\x08\x08', ('192.168.2.255', MOE_PORT))
+        out.sendto(b'\x08\x08\x08\x08', (BROADCAST_IP, MOE_PORT))
         print("*** session has been reloaded ***")
     if query == 'add':
         print("IP address: ", end=" ")
